7e33c04ef9ef3a4358262c40d7d4bcbd3e6f70d1
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-08-18 20:47:18 -0700

    Fibonacci, recursive binary search, GCD

---
+++ src/2019-08-18_basic-algorithms.R
@@ -0,0 +1,90 @@
+
+
+# Fibonacci sequence: ------------------------------
+fib <- function(n){
+    
+    if (n == 0 | n == 1){
+        return(1)
+        
+    } else {
+        return(fib(n-1) + fib(n-2))
+    }
+}
+
+# test: 
+test_vector <- 1:10
+sapply(test_vector, fib)
+
+
+
+# Recursive binary search ------------------------------
+r_binary <- function(A, p, r, x) {
+    # A is a vector (array)
+    # p and r are indexes into the vector 
+    # x is the value we are searching for 
+    
+    # output: index of the point where the result is located 
+    
+    if (p > r) {  # base case 1
+        return("not found")
+        
+    } else {
+        q <- (p + r) %/% 2  # floor division
+        
+        if (A[q] == x) {  # base case 2
+            return(q)
+            
+        } else if (A[q] > x) {
+            return(r_binary(A, p, q-1, x))
+            
+        } else if (A[q] < x) {
+            return(r_binary(A, q+1, r, x))
+            
+        }
+    }
+    
+}
+
+# test: 
+size <- 100000
+A <- c(rnorm(size), 1234)
+r_binary(A, 1, size + 1, 1234)
+
+
+
+# Greatest common divisor: while loop ------------------------------
+gcd <- function(a, b) {
+    while (b != 0) {
+        temp <- b
+        b <- a %% b
+        a <- temp
+    }
+    
+    return(a)
+    
+}
+
+
+# test: 
+gcd(12, 68) == 4
+gcd(12, 68) != 6
+
+gcd(12, 54) == 6
+
+
+
+# Greatest common divisor: recursion ------------------------------
+gcd_recur <- function(a, b) {
+    if (b == 0) {
+        return(a)
+    } else {
+        return(gcd(b, a %% b))
+    }
+}
+
+# test: 
+gcd_recur(12, 68) == 4
+gcd_recur(12, 68) != 5
+gcd_recur(12, 54) == 6
+
+
